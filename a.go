ab72ccae00a0353664594911d0933d443a7c5e33
Rafael Gieschke <rafael@gieschke.de>
2023-12-22 23:31:05 +0000

    -

--- a.go
+++ a.go
@@ -38,8 +38,35 @@ func (v version) process() (versionInfo, error) {
 	}, nil
 }
 
+func getVersion(v version) (versionInfo, error) {
+	return versionInfo{
+		Version: "9999" + v.Component + "+",
+	}, nil
+}
+
+func process2[req, res any](fn func(req) (res, error)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var x1 req
+		if err := json.NewDecoder(r.Body).Decode(&x1); err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		y1, err := fn(x1)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+		fmt.Printf("%#v\n", y1)
+		if err := json.NewEncoder(w).Encode(y1); err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+	}
+}
+
 func main() {
 	http.HandleFunc("/version", process[version])
+	http.HandleFunc("/version2", process2(getVersion))
 	println("aa")
 	http.ListenAndServe(":8080", nil)
 }
